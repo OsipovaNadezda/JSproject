7ca495b81975ffd1c0fa815b6f3e9bbc3122d31e
OsipovaNadezda <osipavanadezda@gmail.com>
2022-06-01 23:16:04 +0300

    First test lesson01

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
-console.log('Hi lesson1101');
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+console.log("let this day be happy");
+
+alert("Have a good day!");
